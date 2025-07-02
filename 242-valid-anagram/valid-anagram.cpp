5f7a131c328ca608d2cd27cc0393837180701e45
Abhishek Bhardwaj <67678682+AbhishekStrix@users.noreply.github.com>
2025-07-02 16:10:13 +0530

    Time: 1 ms (61.33%) | Memory: 9.7 MB (48.69%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,21 +1,10 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        unordered_map<char, int> mp;
-        if (s.length() != t.length())
-            return false;
-        for (char& ch : s) {
-            mp[ch]++;
-        }
-
-        for (char& ch : t) {
-            mp[ch]--;
-
-            if (mp[ch] < 0) {
-                return false;
-            }
-        }
-
-        return true;
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+          return s==t;
+       
     }
+
 };
