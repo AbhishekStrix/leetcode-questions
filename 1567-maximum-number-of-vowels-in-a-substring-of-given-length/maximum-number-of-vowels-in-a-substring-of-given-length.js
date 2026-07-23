9a6ea61068824a82ca8e0110af166214c4c5990b
Abhishek Bhardwaj <67678682+AbhishekStrix@users.noreply.github.com>
2026-07-23 17:39:56 +0530

    Time: 13 ms (87.54%) | Memory: 56.8 MB (47.78%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.js
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.js
@@ -7,17 +7,17 @@ var maxVowels = function (s, k) {
     let count = 0;
     let maxVal = 0;
     for (let i = 0; i < k; i++) {
-        if ('aeiou'.includes(s[i])) {
+        if (s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u') {
             count++
         }
     }
     maxVal = count;
     for (let i = k; i < s.length; i++) {
-       if('aeiou'.includes(s[i-k])){
+       if(s[i-k]=='a' || s[i-k]=='e' || s[i-k]=='i' || s[i-k]=='o' || s[i-k]=='u'){
         count--;
        }
 
-       if('aeiou'.includes(s[i])){
+       if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u'){
         count++;
        }
     maxVal = Math.max(maxVal, count)
